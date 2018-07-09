2d5fde77f38461472e869e8bb44c3d324740c26d
syknapptic <syknapptic@gmail.com>
2018-07-09 16:37:58 -0700

    reticulate-python

--- content/post/py/test_load_pandas.py
+++ content/post/py/test_load_pandas.py
@@ -1,1 +1,18 @@
-import pandas
+def square(x):
+  return x**2
+
+def evens(x):
+  y = []
+  for i in x:
+    if i // 2 == 0:
+      y.append(i)
+  return(y)
+
+
+def ceci_nest_pas_une_pipe(args, *funs):
+  for arg in args:
+    for fun in funs:
+      arg = fun(arg)
+  return arg
+
+ceci_nest_pas_une_pipe([1, 2, 3, 4], even)
